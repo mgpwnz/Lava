647e11063a1338008c0bfa55a748887dd52dd6be
Vitaliy <95574118+mgpwnz@users.noreply.github.com>
2024-05-25 12:49:20 +0300

    update

--- script.sh
+++ script.sh
@@ -24,13 +24,6 @@ bash_profile=$HOME/.bash_profile
 if [ -f "$bash_profile" ]; then
 	    . $HOME/.bash_profile
 fi
-
-export DAEMON_HOME=$HOME/.lava
-export DAEMON_NAME=lavad
-if [ -d "$DAEMON_HOME" ]; then
-    new_folder_name="${DAEMON_HOME}_$(date +"%Y%m%d_%H%M%S")"
-    mv "$DAEMON_HOME" "$new_folder_name"
-fi
 if [ ! $LAVA_ALIAS ]; then
 	read -p "Enter validator name: " LAVA_ALIAS
 	echo 'export LAVA_ALIAS='\"${LAVA_ALIAS}\" >> $HOME/.bash_profile
@@ -40,165 +33,75 @@ echo 'source $HOME/.bashrc' >> $HOME/.bash_profile
 sleep 1
 cd $HOME
 sudo apt update
-sudo apt install make unzip clang pkg-config git-core libudev-dev libssl-dev build-essential libclang-12-dev git jq ncdu bsdmainutils htop -y < "/dev/null"
-sleep 1
-VERSION=1.21.3
-wget -O go.tar.gz https://go.dev/dl/go$VERSION.linux-amd64.tar.gz
-sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go.tar.gz && rm go.tar.gz
-echo 'export GOROOT=/usr/local/go' >> $HOME/.bash_profile
-echo 'export GOPATH=$HOME/go' >> $HOME/.bash_profile
-echo 'export GO111MODULE=on' >> $HOME/.bash_profile
-echo 'export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin' >> $HOME/.bash_profile && . $HOME/.bash_profile
-go version
+sudo apt install -y curl git jq lz4 build-essential
 
-cd $HOME
-rm -rf lava
-git clone https://github.com/lavanet/lava.git
+# Install Go
+sudo rm -rf /usr/local/go
+curl -L https://go.dev/dl/go1.21.6.linux-amd64.tar.gz | sudo tar -xzf - -C /usr/local
+echo 'export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin' >> $HOME/.bash_profile
+source .bash_profile
+
+cd && rm -rf lava
+git clone https://github.com/lavanet/lava
 cd lava
-git checkout v2.0.1
+git checkout v2.0.0
+
+# Build binary
 export LAVA_BINARY=lavad
 make install
 
-$DAEMON_NAME config chain-id lava-testnet-2
-$DAEMON_NAME config keyring-backend test
-$DAEMON_NAME config node tcp://localhost:19957
+# Set node CLI configuration
+lavad config chain-id lava-testnet-2
+lavad config keyring-backend test
+lavad config node tcp://localhost:19957
 
 # Initialize the node
-$DAEMON_NAME init "$MONIKER" --chain-id lava-testnet-2
+lavad init "$OG_ALIAS" --chain-id lava-testnet-2
 
 # Download genesis and addrbook files
-curl -L https://snapshots-testnet.nodejumper.io/lava-testnet/genesis.json > $DAEMON_HOME/config/genesis.json
-curl -L https://snapshots-testnet.nodejumper.io/lava-testnet/addrbook.json > $DAEMON_HOME/config/addrbook.json
+curl -L https://snapshots-testnet.nodejumper.io/lava-testnet/genesis.json > $HOME/.lava/config/genesis.json
+curl -L https://snapshots-testnet.nodejumper.io/lava-testnet/addrbook.json > $HOME/.lava/config/addrbook.json
 
 # Set seeds
 sed -i -e 's|^seeds *=.*|seeds = "3a445bfdbe2d0c8ee82461633aa3af31bc2b4dc0@prod-pnet-seed-node.lavanet.xyz:26656,e593c7a9ca61f5616119d6beb5bd8ef5dd28d62d@prod-pnet-seed-node2.lavanet.xyz:26656,ade4d8bc8cbe014af6ebdf3cb7b1e9ad36f412c0@testnet-seeds.polkachu.com:19956,eb7832932626c1c636d16e0beb49e0e4498fbd5e@lava-testnet-seed.itrocket.net:20656"|' $HOME/.lava/config/config.toml
 
 # Set minimum gas price
-sed -i -e "s|^minimum-gas-prices *=.*|minimum-gas-prices = \"0ulava\"|" $HOME/.lava/config/app.toml
+sed -i -e 's|^minimum-gas-prices *=.*|minimum-gas-prices = "0.000001ulava"|' $HOME/.lava/config/app.toml
 
 # Set pruning
 sed -i \
   -e 's|^pruning *=.*|pruning = "custom"|' \
   -e 's|^pruning-keep-recent *=.*|pruning-keep-recent = "100"|' \
   -e 's|^pruning-interval *=.*|pruning-interval = "17"|' \
-  $DAEMON_HOME/config/app.toml
+  $HOME/.lava/config/app.toml
 
+# Change ports
+sed -i -e "s%:1317%:19917%; s%:8080%:19980%; s%:9090%:19990%; s%:9091%:19991%; s%:8545%:19945%; s%:8546%:19946%; s%:6065%:19965%" $HOME/.lava/config/app.toml
+sed -i -e "s%:26658%:19958%; s%:26657%:19957%; s%:6060%:19960%; s%:26656%:19956%; s%:26660%:19961%" $HOME/.lava/config/config.toml
 
-echo "[Unit]
-Description=$NODE Node
-After=network.target
+# Download latest chain data snapshot
+curl "https://snapshots-testnet.nodejumper.io/lava-testnet/lava-testnet_latest.tar.lz4" | lz4 -dc - | tar -xf - -C "$HOME/.lava"
 
+# Create a service
+sudo tee /etc/systemd/system/lava.service > /dev/null << EOF
+[Unit]
+Description=Lava node service
+After=network-online.target
 [Service]
 User=$USER
-Type=simple
 ExecStart=$(which lavad) start
 Restart=on-failure
+RestartSec=10
 LimitNOFILE=65535
-
 [Install]
-WantedBy=multi-user.target" > $HOME/$NODE.service
-sudo mv $HOME/$NODE.service /etc/systemd/system
-sudo tee <<EOF >/dev/null /etc/systemd/journald.conf
-Storage=persistent
+WantedBy=multi-user.target
 EOF
-
-curl "https://snapshots-testnet.nodejumper.io/lava-testnet/lava-testnet_latest.tar.lz4" | lz4 -dc - | tar -xf - -C "$DAEMON_HOME"
-
-echo -e '\n\e[42mChecking a ports\e[0m\n' && sleep 1
-#CHECK PORTS
-PORT=345
-if ss -tulpen | awk '{print $5}' | grep -q ":26656$" ; then
-    echo -e "\e[31mPort 26656 already in use.\e[39m"
-    sleep 2
-    sed -i -e "s|:26656\"|:${PORT}56\"|g" $DAEMON_HOME/config/config.toml
-    echo -e "\n\e[42mPort 26656 changed to ${PORT}56.\e[0m\n"
-    sleep 2
-fi
-if ss -tulpen | awk '{print $5}' | grep -q ":26657$" ; then
-    echo -e "\e[31mPort 26657 already in use\e[39m"
-    sleep 2
-    sed -i -e "s|:26657\"|:${PORT}57\"|" $DAEMON_HOME/config/config.toml
-    echo -e "\n\e[42mPort 26657 changed to ${PORT}57.\e[0m\n"
-    sleep 2
-    $DAEMON_NAME config node tcp://localhost:${PORT}57
-fi
-if ss -tulpen | awk '{print $5}' | grep -q ":26658$" ; then
-    echo -e "\e[31mPort 26658 already in use.\e[39m"
-    sleep 2
-    sed -i -e "s|:26658\"|:${PORT}58\"|" $DAEMON_HOME/config/config.toml
-    echo -e "\n\e[42mPort 26658 changed to ${PORT}58.\e[0m\n"
-    sleep 2
-fi
-if ss -tulpen | awk '{print $5}' | grep -q ":6060$" ; then
-    echo -e "\e[31mPort 6060 already in use.\e[39m"
-    sleep 2
-    sed -i -e "s|:6060\"|:${PORT}60\"|" $DAEMON_HOME/config/config.toml
-    echo -e "\n\e[42mPort 6060 changed to ${PORT}60.\e[0m\n"
-    sleep 2
-fi
-if ss -tulpen | awk '{print $5}' | grep -q ":1317$" ; then
-    echo -e "\e[31mPort 1317 already in use.\e[39m"
-    sleep 2
-    sed -i -e "s|:1317\"|:${PORT}17\"|" $DAEMON_HOME/config/app.toml
-    echo -e "\n\e[42mPort 1317 changed to ${PORT}17.\e[0m\n"
-    sleep 2
-fi
-if ss -tulpen | awk '{print $5}' | grep -q ":9090$" ; then
-    echo -e "\e[31mPort 9090 already in use.\e[39m"
-    sleep 2
-    sed -i -e "s|:9090\"|:${PORT}90\"|" $DAEMON_HOME/config/app.toml
-    echo -e "\n\e[42mPort 9090 changed to ${PORT}90.\e[0m\n"
-    sleep 2
-fi
-if ss -tulpen | awk '{print $5}' | grep -q ":9091$" ; then
-    echo -e "\e[31mPort 9091 already in use.\e[39m"
-    sleep 2
-    sed -i -e "s|:9091\"|:${PORT}91\"|" $DAEMON_HOME/config/app.toml
-    echo -e "\n\e[42mPort 9091 changed to ${PORT}91.\e[0m\n"
-    sleep 2
-fi
-if ss -tulpen | awk '{print $5}' | grep -q ":8545$" ; then
-    echo -e "\e[31mPort 8545 already in use.\e[39m"
-    sleep 2
-    sed -i -e "s|:8545\"|:${PORT}45\"|" $DAEMON_HOME/config/app.toml
-    echo -e "\n\e[42mPort 8545 changed to ${PORT}45.\e[0m\n"
-    sleep 2
-fi
-if ss -tulpen | awk '{print $5}' | grep -q ":8546$" ; then
-    echo -e "\e[31mPort 8546 already in use.\e[39m"
-    sleep 2
-    sed -i -e "s|:8546\"|:${PORT}46\"|" $DAEMON_HOME/config/app.toml
-    echo -e "\n\e[42mPort 8546 changed to ${PORT}46.\e[0m\n"
-    sleep 2
-fi
-if ss -tulpen | awk '{print $5}' | grep -q ":8080$" ; then
-    echo -e "\e[31mPort 8080 already in use.\e[39m"
-    sleep 2
-    sed -i -e "s|:8080\"|:${PORT}80\"|" $DAEMON_HOME/config/app.toml
-    echo -e "\n\e[42mPort 8080 changed to ${PORT}80.\e[0m\n"
-    sleep 2
-fi
-if ss -tulpen | awk '{print $5}' | grep -q ":6065$" ; then
-    echo -e "\e[31mPort 6065 already in use.\e[39m"
-    sleep 2
-    sed -i -e "s|:6065\"|:${PORT}65\"|" $DAEMON_HOME/config/config.toml
-    echo -e "\n\e[42mPort 6065 changed to ${PORT}65.\e[0m\n"
-    sleep 2
-fi
-echo -e '\n\e[42mRunning a service\e[0m\n' && sleep 1
-sudo systemctl restart systemd-journald
 sudo systemctl daemon-reload
-sudo systemctl enable $NODE
-sudo systemctl restart $NODE
-cd $HOME
-echo -e '\n\e[42mCheck node status\e[0m\n' && sleep 1
-if [[ `service $NODE status | grep active` =~ "running" ]]; then
-  echo -e "Your $NODE node \e[32minstalled and works\e[39m!"
-  echo -e "You can check node status by the command \e[7mservice $NODE status\e[0m"
-  echo -e "Press \e[7mQ\e[0m for exit from status menu"
-else
-  echo -e "Your $NODE node \e[31mwas not installed correctly\e[39m, please reinstall."
-fi
+sudo systemctl enable lava.service
+
+# Start the service and check the logs
+sudo systemctl start lava.service
+sudo journalctl -u lava.service -f --no-hostname -o cat
 }
 uninstall() {
 read -r -p "You really want to delete the node? [y/N] " response
